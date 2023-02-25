2cb2d5a9dbe789d09c65fc3828d03b5e6c12260c
Евгений <Grekos_89@mail.ru>
2023-02-25 18:17:22 +0300

    созданы функции

--- task/Program.cs
+++ task/Program.cs
@@ -1,3 +1,25 @@
 ﻿string[] arr = new string[] {"фамилия", "имя", "отчество", "дом", "этаж", "квартира", "123"};
 string[] arr2 = new string[arr.Length];
 
+void ThreeSymbolInArray(string[] arr, string[] arr2)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3)
+        {
+            arr2[count] = arr[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    
+    foreach(string item in arr)
+    {
+        System.Console.Write($"{item}, ");
+    } 
+    System.Console.WriteLine();
+}
